2ae99ee4a22f5643cf5cbd51a088026777a52793
guiarthur <arthur.guillaume@ynov.com>
2026-02-27 16:37:37 +0100

    msg

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(n1) {
+    return n1 + 1
+}
+
+function less(n1) {
+    return n1 - 1
+}
+
+function add(n1, n2) {
+    return n1 + n2
+}
+
+function sub(n1, n2) {
+    return n1 - n2
+}
