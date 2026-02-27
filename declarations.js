d3de996d65562f219b2b822a6d1799cf9e87be8a
guiarthur <arthur.guillaume@ynov.com>
2026-02-27 15:21:50 +0100

    msg

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = "`\\/\"'";
+const arr = [4, '2']
+const obj = Object.freeze({
+    str : "zobhumid",
+    num : 7,
+    bool : true,
+    undef : undefined,
+})
+const nested = Object.freeze({
+    arr : [4, undefined, '2'],
+    obj : {
+        str : "zboubynueve",
+        num : 7,
+        bool : true,
+    }
+})
