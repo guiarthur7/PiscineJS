2b68b3a50c07b148295407700f060ffda90e1133
guiarthur <arthur.guillaume@ynov.com>
2026-02-27 17:34:11 +0100

    msg

--- dog-years.js
+++ dog-years.js
@@ -1,30 +1,35 @@
 function dogYears(planet, age) {
+    const earthYear = 31557600; // secondes par année terrestre
     let orbitalPeriod;
     
     if (planet === "earth") {
         orbitalPeriod = 1.0;
     }
     if (planet === "mercury") {
-        orbitalPeriod = 0.2408467 * 31536000;
+        orbitalPeriod = 0.2408467;
     }
     if (planet === "venus") {
-        orbitalPeriod = 0.61519726 * 31536000;
+        orbitalPeriod = 0.61519726;
     }
     if (planet === "mars") {
-        orbitalPeriod = 1.8808158 * 31536000;
+        orbitalPeriod = 1.8808158;
     }
     if (planet === "jupiter") {
-        orbitalPeriod = 11.862615 * 31536000;
+        orbitalPeriod = 11.862615;
     }
     if (planet === "saturn") {
-        orbitalPeriod = 29.447498 * 31536000;
+        orbitalPeriod = 29.447498;
     }
     if (planet === "uranus") {
-        orbitalPeriod = 84.016846 * 31536000;
+        orbitalPeriod = 84.016846;
     }
     if (planet === "neptune") {
-        orbitalPeriod = 164.79132 * 31536000;
+        orbitalPeriod = 164.79132;
     }
     
-    return age / orbitalPeriod;
+    const ageInEarthYears = age / earthYear;
+    const ageOnPlanet = ageInEarthYears / orbitalPeriod;
+    const dogAge = ageOnPlanet * 7;
+    
+    return Math.round(dogAge * 100) / 100;
 }
