26ad2edeb25833e67a92c235f3c5f76d51d3dfec
guiarthur <arthur.guillaume@ynov.com>
2026-03-02 09:38:02 +0100

    msg

---
+++ method-man.js
@@ -0,0 +1,36 @@
+function word(s) {
+    let tab = []
+    let str = ""
+    for (let i = 0; i < s.Length - 1; i++) {
+        if (s[i] == " ") {
+            tab.push(str)
+            str = ""
+        } else {
+            str = str + s[i]
+        }
+    }
+    return tab
+}
+
+function sentence(tab) {
+    let res = ""
+    for (let i = 0; i < tab.Length -1; i++) {
+        res += tab[i] + " "
+    }
+    return res
+}
+
+function yell(s) {
+    return s.ToLowerCase()
+}
+
+function whisper(s) {
+    res = s.ToLowerCase()
+    return "*" + res + "*"
+}
+
+function capitalize(s) {
+    res = ""
+    res += s[1].ToUpperCase() + s.slice(1).ToLowerCase()
+    return res
+}
