4c732c52c0cf2d50fa7c5dce84d46ecaceccde81
guiarthur <arthur.guillaume@ynov.com>
2026-03-02 09:54:10 +0100

    msg

---
+++ abs.js
@@ -0,0 +1,15 @@
+function isPositive(n) {
+    if (n > 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function abs(n) {
+    if (n >= 0) {
+        return n
+    } else {
+        return -n
+    }
+}
