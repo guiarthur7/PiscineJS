c740f3f3fc4914398c1ad2553149dcc1c915211c
guiarthur <arthur.guillaume@ynov.com>
2026-03-02 12:00:47 +0100

    msg

--- physics.js
+++ physics.js
@@ -1,12 +1,12 @@
 function getAcceleration(obj) {
     let a = 0
-    if (obj.F !== undifined && obj.m !== undifined) {
+    if (obj.F !== undefined && obj.m !== undefined) {
         a = obj.F/obj.m
         return a
-    } if (obj.Δt !== undifined && obj.Δv !== undifined) {
+    } if (obj.Δt !== undefined && obj.Δv !== undefined) {
         a = obj.Δt/obj.Δv
         return a
-    } if (obj.d !== undifined && obj.t !== undifined) {
+    } if (obj.d !== undefined && obj.t !== undefined) {
         a = 2*obj.d/obj.t**2
         return a
     } else {
