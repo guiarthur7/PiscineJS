fd2db0158762c7caa9fe20f047453cfa0fe5546f
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 11:37:19 +0100

    msg

--- slicer.js
+++ slicer.js
@@ -1,7 +1,7 @@
 function slice(s, fromindex, lastindex = s.length) {
     let res = ""
     for (let i= fromindex; i<=lastindex; i++) {
-        res += String(tab[i])
+        res += String(s[i])
     }
     return res
 }
