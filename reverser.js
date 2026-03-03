31fd77d868be5f9145bb15fa9f9fe25ea225cfe6
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 12:42:43 +0100

    msg

--- reverser.js
+++ reverser.js
@@ -1,13 +1,13 @@
 function reverse(s) {
     if (typeof s === 'string') {
         let res = ""
-        for (let i = s.length - 1; i >= 0; i++) {
+        for (let i = s.length - 1; i >= 0; i--) {
             res += s[i]
         }
         return res
     } if (Array.isArray(s)){
         let res = []
-        for (let i = s.length - 1; i >= 0; i++) {
+        for (let i = s.length - 1; i >= 0; i--) {
             res.push(s[i])
         }
         return res
