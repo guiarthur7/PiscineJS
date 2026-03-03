65b4dd25b25726bbab91fe8eb4d62f2927d1de37
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 11:58:34 +0100

    msg

--- slicer.js
+++ slicer.js
@@ -2,7 +2,7 @@ function slice(s, fromindex, lastindex = s.length) {
     let res = ""
     if (fromindex < 0) {
         fromindex = (s.length) + fromindex 
-    } else if (lastindex < 0) {
+    } if (lastindex < 0) {
         lastindex = (s.length) + lastindex 
     }
     for (let i= fromindex; i<lastindex; i++) {
