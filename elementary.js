0abdffeb31b0f992cd6c0d93fbeff89268042c79
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 09:41:02 +0100

    msg

--- elementary.js
+++ elementary.js
@@ -23,27 +23,25 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    let res = 0
-    if (a === 0 || b === 0) {
-        throw Error;
-    } if (a < 0 && b > 0) {
-        for (let i = 0; i <b; i++) {
-            res -= -a
-        }
-    } if (a > 0 && b < 0) {
-        for (let i = 0; i <a; i++) {
-            res -= -b
-        }
-    } if (a < 0 && b < 0) {
-        for (let i = 0; i < -a; i++) {
-            res -= -b
-        }
-    } if (a > 0 && b > 0) {
-        for (let i = 0; i < a; i++) {
-            res -= b
-        }
-    }    
-    return res
+    if (b === 0) {
+        throw new Error();
+    }
+    
+    if (a === 0) {
+        return 0;
+    }
+    
+    let res = 0;
+    let isNegative = (a < 0) !== (b < 0);
+    let absA = a < 0 ? -a : a;
+    let absB = b < 0 ? -b : b;
+    
+    while (absA >= absB) {
+        absA -= absB;
+        res++;
+    }
+    
+    return isNegative ? -res : res;
 }
 
 function modulo(a, b) {
