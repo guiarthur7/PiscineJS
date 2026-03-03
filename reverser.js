ff3a17f90d71c557caf06551b8fc2b5df3750113
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 12:41:48 +0100

    msg

--- reverser.js
+++ reverser.js
@@ -4,13 +4,14 @@ function reverse(s) {
         for (let i = s.length - 1; i >= 0; i++) {
             res += s[i]
         }
+        return res
     } if (Array.isArray(s)){
         let res = []
         for (let i = s.length - 1; i >= 0; i++) {
             res.push(s[i])
         }
+        return res
     } else {
         throw Error
     }
-    return res
 }
