58062da2d601998ce7e31c40080542caf5014936
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 12:16:06 +0100

    msg

---
+++ keep-cut.js
@@ -0,0 +1,15 @@
+function cutFirst(s) {
+    return s.slice(2, s.length)
+}
+
+function cutLast(s) {
+    return s.slice(0, s.length - 2)
+}
+
+function cutFirstLast(s) {
+    return s.slice(2, s.length - 2)
+}
+
+function keepFirst(s) {
+    
+}
