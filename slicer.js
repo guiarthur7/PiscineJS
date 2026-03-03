a05f6e9b3a8eb1f8e9745d7dddd06fe0d72521cf
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 11:54:10 +0100

    msg

--- slicer.js
+++ slicer.js
@@ -2,8 +2,8 @@ function slice(s, fromindex, lastindex = s.length) {
     let res = ""
     if (fromindex < 0) {
         fromindex = (s.length) + fromindex
-    } else if (Lastindex < 0) {
-        Lastindex = (s.length) + Lastindex
+    } else if (lastindex < 0) {
+        lastindex = (s.length) + Lastindex
     }
     for (let i= fromindex; i<lastindex; i++) {
         res += String(s[i])
