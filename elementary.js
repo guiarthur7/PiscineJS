edee488a5096ed7c7768687c7b6024329ee273e5
guiarthur <arthur.guillaume@ynov.com>
2026-03-03 09:35:39 +0100

    msg

---
+++ elementary.js
@@ -0,0 +1,54 @@
+function multiply(a, b) {
+    let res = 0
+    if (a === 0 || b === 0) {
+        return 0
+    } if (a < 0 && b > 0) {
+        for (let i = 0; i <b; i++) {
+            res += -a
+        }
+    } if (a > 0 && b < 0) {
+        for (let i = 0; i <a; i++) {
+            res += -b
+        }
+    } if (a < 0 && b < 0) {
+        for (let i = 0; i < -a; i++) {
+            res += -b
+        }
+    } if (a > 0 && b > 0) {
+        for (let i = 0; i < a; i++) {
+            res += b
+        }
+    }    
+    return res
+}
+
+function divide(a, b) {
+    let res = 0
+    if (a === 0 || b === 0) {
+        throw Error;
+    } if (a < 0 && b > 0) {
+        for (let i = 0; i <b; i++) {
+            res -= -a
+        }
+    } if (a > 0 && b < 0) {
+        for (let i = 0; i <a; i++) {
+            res -= -b
+        }
+    } if (a < 0 && b < 0) {
+        for (let i = 0; i < -a; i++) {
+            res -= -b
+        }
+    } if (a > 0 && b > 0) {
+        for (let i = 0; i < a; i++) {
+            res -= b
+        }
+    }    
+    return res
+}
+
+function modulo(a, b) {
+    while (a > b) {
+        a -= b
+    }
+    return b
+}
