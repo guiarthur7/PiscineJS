a762dc9082175c1260124c189e79537eb9a26e34
guiarthur <arthur.guillaume@ynov.com>
2026-03-05 20:02:11 +0100

    msg

---
+++ chunk.js
@@ -0,0 +1,20 @@
+function chunk(tab, d) {
+    let res = []
+    let tab2 = []
+    let d1 = 0
+    for (let i = 0; i < tab.length; i++) {
+        if (d1 < d) {
+            tab2.push(tab[i])
+            d1 += 1
+        } else {
+            res.push(tab2)
+            tab2 = []
+            d1 = 1
+        }
+    }
+    res.push(tab2)
+    return res
+}
+
+console.log(chunk(["a", "b", "c", "d"], 2));
+console.log(chunk(["a", "b", "c", "d", "e"], 2));
