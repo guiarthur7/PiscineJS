6d7d0eddc16aab2d026706872f1a3b4ba1fc6874
guiarthur <arthur.guillaume@ynov.com>
2026-03-05 20:19:12 +0100

    msg

--- nasa.js
+++ nasa.js
@@ -1,6 +1,6 @@
 function nasa(n) {
     let res = ""
-    for (let i = 0; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         if (i%5 === 0 && i%3 === 0) {
             res += "NASA "
         } else if (i%3 === 0) {
