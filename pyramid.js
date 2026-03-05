baef6df7a61319fbe271a0d4130b70e4c76e3717
guiarthur <arthur.guillaume@ynov.com>
2026-03-05 21:17:35 +0100

    msg

--- pyramid.js
+++ pyramid.js
@@ -2,7 +2,7 @@ function pyramid(s, h) {
     let res = ""
     let space = " "
     for (let i = 1; i <= h; i++) {
-        res += space.repeat(h-i) + s.repeat(2*i-1)
+        res += space.repeat((h-i) * s.length) + s.repeat(2*i-1)
         if (i !== h) {
             res += '\n'
         }
