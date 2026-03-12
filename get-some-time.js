7a2534f8c7aceead596343b4a6dceb43f75348cc
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 14:31:38 +0100

    msg

--- get-some-time.js
+++ get-some-time.js
@@ -1,25 +1,30 @@
 function firstDayWeek(n, s) {
-    const d = new Date(s + "-01-01")
-    const day = d.getDay()
-    let diff = 0
+    const d = new Date(s + "-01-01");
+    const day = d.getDay();
+    let diff;
     if (day === 0) {
-        diff = 1
-    } if (day === 1) {
-        diff = 0
+        diff = 1;
+    } else if (day === 1) {
+        diff = 0;
     } else {
-        diff = 8 - day
+        diff = 8 - day;
     }
     if (n === 1) {
-        return "01-01-" + s
+        return "01-01-" + s;
     }
     let lundi = new Date(d);
-    lundi.setDate(d.getDate() + diff + 7 * (n - 2));
-
-    let m1 = lundi.getMonth() + 1
-    if (m1<10) {m1 = '0' + m1}
-    let d2 = lundi.getDate()
-    if (d2<10) {d2 = '0' + d2}
-    return d2 + "-" + m1 + "-" + s
+    if (diff === 0) {
+        // 1er janvier est un lundi, semaine 2 = 8 janvier
+        lundi.setDate(d.getDate() + 7 * (n - 1));
+    } else {
+        // sinon, semaine 2 = premier lundi après le 1er janvier
+        lundi.setDate(d.getDate() + diff + 7 * (n - 2));
+    }
+    let m1 = lundi.getMonth() + 1;
+    if (m1 < 10) { m1 = '0' + m1; }
+    let d2 = lundi.getDate();
+    if (d2 < 10) { d2 = '0' + d2; }
+    return d2 + "-" + m1 + "-" + s;
 }
 
 console.log(firstDayWeek(1, "2024"));
