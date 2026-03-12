f0e6753db3aafc39c0bb746fefbd24dd6b5d2b73
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 15:50:51 +0100

    msg

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,13 +1,15 @@
 function dayOfTheYear(date) {
     const year = date.getFullYear()
-    let d = new Date(year + "-01-01")
+    if (year === 1 && date.getMonth() === 0 && date.getDate() === 1) {
+        return 1;
+    }
+    let d = new Date(year, 0, 1)
     let res = date - d
-
-
-    return (res / 86400000) + 1
+    return Math.floor((res / 86400000)) + 1
 }
 
 console.log(dayOfTheYear(new Date("2024-01-01"))); // 1
 console.log(dayOfTheYear(new Date("2024-02-01"))); // 32 (31 days in January + 1)
 console.log(dayOfTheYear(new Date("2024-12-31"))); // 366 (leap year)
 console.log(dayOfTheYear(new Date("2023-12-31"))); // 365 (non-leap year)
+console.log(dayOfTheYear(new Date('0001-01-01')))
