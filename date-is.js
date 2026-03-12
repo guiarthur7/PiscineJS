51ff17859616cf5a3a7443cc30f60cbb899078f5
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 11:46:30 +0100

    msg

--- date-is.js
+++ date-is.js
@@ -17,11 +17,3 @@ function isAfter(d1, d2) {
         }
     }
 }
-
-const date1 = new Date("2024-01-01");
-const date2 = new Date("2023-01-01");
-const invalid = new Date("invalid");
-
-console.log(isValid(date1)); // true
-console.log(isValid(invalid)); // false
-console.log(isAfter(date1, date2)); // true
