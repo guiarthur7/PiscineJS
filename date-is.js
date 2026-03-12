82fd2817573f184eed40b637b97f02e2be5b6071
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 11:46:07 +0100

    msg

--- date-is.js
+++ date-is.js
@@ -9,11 +9,19 @@ function isValid(date) {
 }
 
 function isAfter(d1, d2) {
-    if (d1.getTime() - d2.getTime() > 0) {
-        return true
-    } else {
-        return false
+    if (isValid(d1) && isValid(d2)) {
+        if (d1.getTime() - d2.getTime() > 0) {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
-console.log(isValid(Date.now()))
+const date1 = new Date("2024-01-01");
+const date2 = new Date("2023-01-01");
+const invalid = new Date("invalid");
+
+console.log(isValid(date1)); // true
+console.log(isValid(invalid)); // false
+console.log(isAfter(date1, date2)); // true
