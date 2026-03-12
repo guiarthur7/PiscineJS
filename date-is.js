c3fcdbbb84cdc29be4412f4504758be867daf611
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 11:25:05 +0100

    msg

--- date-is.js
+++ date-is.js
@@ -5,11 +5,3 @@ function isValid(date) {
         return true
     }
 }
-
-
-const date1 = new Date("2024-01-01");
-const date2 = new Date("2023-01-01");
-const invalid = new Date("invalid");
-
-console.log(isValid(date1)); // true
-console.log(isValid(invalid)); // false
