4d61abdcf772f62fcd5ffaa861a47310b9415849
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 11:50:11 +0100

    msg

--- date-is.js
+++ date-is.js
@@ -21,4 +21,17 @@ function isAfter(d1, d2) {
     }
 }
 
+function isBefor(d1, d2) {
+    if (isValid(d1) && isValid(d2)) {
+        if (typeof d1 === "number" && typeof d2 === "number") {
+            return d1 - d2 < 0
+        }
+        if (d1.getTime() - d2.getTime() < 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
 console.log(isAfter(123123, 526))
