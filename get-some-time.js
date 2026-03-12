96dc9cf7227d7e708aeab12d4be6c98ff0b9208b
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 12:21:16 +0100

    msg

--- get-some-time.js
+++ get-some-time.js
@@ -1,23 +1,31 @@
 function firstDayWeek(n, s) {
-    // Crée la date du 1er janvier de l'année s
-    const d = new Date(s + "-01-01");
-    // Jour de la semaine (0=dimanche, 1=lundi, ...)
-    let day = d.getDay();
-    // Calcul du décalage pour obtenir le lundi
-    let offset = (day === 0) ? 1 : (8 - day);
-    // Date du premier lundi de l'année
-    let firstMonday = new Date(d);
+    var d = new Date(s + "-01-01");
+    if (n === 1) {
+        var dd = d.getDate();
+        if (dd < 10) dd = '0' + dd;
+        var mm = d.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+        var yyyy = d.getFullYear();
+        return dd + '-' + mm + '-' + yyyy;
+    }
+    var day = d.getDay();
+    var offset;
+    if (day === 0) {
+        offset = 1;
+    } else {
+        offset = 8 - day;
+    }
+    var firstMonday = new Date(d);
     firstMonday.setDate(d.getDate() + offset - 1);
-    // Date du lundi de la semaine n
-    let mondayN = new Date(firstMonday);
+    var mondayN = new Date(firstMonday);
     mondayN.setDate(firstMonday.getDate() + 7 * (n - 1));
-    // Si la semaine commence l'année précédente, retourne le 1er janvier
     if (mondayN.getFullYear() < Number(s)) {
         mondayN = d;
     }
-    // Formatage en dd-mm-yyyy
-    const dd = String(mondayN.getDate()).padStart(2, '0');
-    const mm = String(mondayN.getMonth() + 1).padStart(2, '0');
-    const yyyy = mondayN.getFullYear();
-    return `${dd}-${mm}-${yyyy}`;
+    var dd = mondayN.getDate();
+    if (dd < 10) dd = '0' + dd;
+    var mm = mondayN.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+    var yyyy = mondayN.getFullYear();
+    return dd + '-' + mm + '-' + yyyy;
 }
