b8bab5a37df75ab5afff2bc3d74d7bac5e60ba03
guiarthur <arthur.guillaume@ynov.com>
2026-03-12 14:22:28 +0100

    msg

--- get-some-time.js
+++ get-some-time.js
@@ -7,7 +7,7 @@ function firstDayWeek(n, s) {
     } if (day === 1) {
         diff = 0
     } else {
-        diff = 7 - day
+        diff = 8 - day
     }
     if (n === 1) {
         let m = d.getMonth() + 1
@@ -25,3 +25,12 @@ function firstDayWeek(n, s) {
     if (d2<10) {d2 = '0' + d2}
     return d2 + "-" + m1 + "-" + s
 }
+
+console.log(firstDayWeek(1, "2024"));
+// Output: '01-01-2024'  (since week 1 contains January 1st)
+
+console.log(firstDayWeek(10, "2025"));
+// Output: '03-03-2025'  (the Monday of week 10 in 2025)
+
+console.log(firstDayWeek(1, "2021"));
+// Output: '01-01-2021'  (week 1 starts on Friday, but we return Jan 1st)
