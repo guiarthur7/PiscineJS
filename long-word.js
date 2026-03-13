526c7d8e588eede0f7079f795866d816ab449b97
guiarthur <arthur.guillaume@ynov.com>
2026-03-13 12:49:33 +0100

    msg

---
+++ long-word.js
@@ -0,0 +1,17 @@
+function longWord(tab) {
+    return tab.every(word => word.length >= 5)
+}
+
+console.log(longWord(["IFBUAUFAUZFAFUI", "AIOZFBOAUFBAUOFBAOUFB"]))
+console.log(longWord(["FUI", "FB"]))
+
+function oneLongWord(tab) {
+    return tab.some(word => word.length >= 10)
+}
+
+console.log(oneLongWord(["iuzfbvaiufaufaf", "uadbgbrguijub"]))
+console.log(oneLongWord(["ufaf", "uaub"]))
+
+function noLongWords(tab) {
+    return tab.every(word => word.length <= 7)
+}
