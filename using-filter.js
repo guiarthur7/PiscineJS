69298abad68bd97b23a58943c6d1cb0b06ac6d30
guiarthur <arthur.guillaume@ynov.com>
2026-03-13 12:03:33 +0100

    msg

--- using-filter.js
+++ using-filter.js
@@ -1,5 +1,5 @@
 function filterShortStateName(tab) {
-    return tab.filter(word => word.length > 7)
+    return tab.filter(word => word.length > 6)
 }
 
 console.log(filterShortStateName(["Salutcarthur", "aa"]))
