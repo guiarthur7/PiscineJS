5e7631cabaf1b6506d96e55a8a33c610b6e3e00b
guiarthur <arthur.guillaume@ynov.com>
2026-03-13 10:50:29 +0100

    msg

--- using-map.js
+++ using-map.js
@@ -4,11 +4,42 @@ function citiesOnly(tab) {
 }
 
 function upperCasingStates(tab) {
-    const states = tab.map(obj => 
-        obj[0].toUpperCase() + obj.slice(1)
-    )
-    return states
+  return tab.map(state =>
+    state
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+  );
 }
+
+function trimTemp(tab) {
+    return tab.map(({ city, temperature }) => ({
+        city: city,
+        temperature: temperature.replace(/\s+/g, "")
+    }));
+}
+
+function tempForecasts(tab) {
+  return tab.map(({ city, temperature, state, region }) => {
+    const tempF = parseFloat(temperature.replace(/[^0-9.\-]/g, ""));
+    const tempC = ((tempF - 32) * 5 / 9).toFixed(1);
+    return `${tempC}°Celsius in ${city}, ${state}`;
+  });
+}
+
+console.log(tempForecasts([
+  {
+    city: "Pasadena",
+    temperature: " 101 °F",
+    state: "california",
+    region: "West",
+  },
+]));
+
+console.log(trimTemp([
+  { city: "Los Angeles", temperature: "  101 °F   " },
+  { city: "San Francisco", temperature: " 84 ° F   " },
+]));
 
 console.log(citiesOnly([
   {
