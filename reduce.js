34413ecad9a1c536bb989e5e695d0e0f2083cecb
guiarthur <arthur.guillaume@ynov.com>
2026-03-16 21:06:10 +0100

    msg

--- reduce.js
+++ reduce.js
@@ -1,13 +1,15 @@
-function fold(tab, func, n) {
-    tab.unshift(n)
+
+function fold(tab, func, initial) {
+    let acc = initial
     for (let i = 0; i < tab.length; i++) {
         acc = func(acc, tab[i], i, tab)
     }
     return acc
 }
 
-function foldRight(tab, func, n) {
-    tab.unshift(n)
+
+function foldRight(tab, func, initial) {
+    let acc = initial
     for (let i = tab.length - 1; i >= 0; i--) {
         acc = func(acc, tab[i], i, tab)
     }
