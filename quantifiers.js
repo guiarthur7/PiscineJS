59aacff31fbc2d23560d2b3188a0446f64efe756
guiarthur <arthur.guillaume@ynov.com>
2026-03-16 21:20:40 +0100

    msg

---
+++ quantifiers.js
@@ -0,0 +1,41 @@
+function every(tab, func) {
+    let n = 0 
+    for (let i=0;i<tab.length;i++) {
+        if (func(tab[i], i, tab)) {
+            n++
+        }
+    }
+    if (n === tab.length - 1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function some(tab, func) {
+    let n = 0 
+    for (let i=0;i<tab.length;i++) {
+        if (func(tab[i], i, tab)) {
+            n++
+        }
+    }
+    if (n >= 1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function none(tab, func) {
+    let n = 0 
+    for (let i=0;i<tab.length;i++) {
+        if (func(tab[i], i, tab)) {
+            n++
+        }
+    }
+    if (n === 0) {
+        return true
+    } else {
+        return false
+    }
+}
