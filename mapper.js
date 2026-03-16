a3d25b5fcde23d58aadc0d5eb823c267266c71bb
guiarthur <arthur.guillaume@ynov.com>
2026-03-16 19:25:03 +0100

    msg

--- mapper.js
+++ mapper.js
@@ -5,3 +5,17 @@ function map(tab, func) {
     }
     return res
 }
+
+function flatMap(tab, func) {
+    let res = []
+    for (let i = 0; i < tab.length; i++) {
+        res.push(func(tab[i], i, tab))
+    }
+    let res2 = []
+    for (let x = 0; x < res.length; x++) {
+        for (y = 0; y < res[x].length; y++) {
+            res2.push(res[x][y])
+        }
+    }
+    return res2
+}
