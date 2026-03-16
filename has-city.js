fa17fa5348dd7aa1a90e9e0453c3b60d8e982679
guiarthur <arthur.guillaume@ynov.com>
2026-03-16 21:34:40 +0100

    msg

--- has-city.js
+++ has-city.js
@@ -1,9 +1,9 @@
 function hasCity(country, tab) {
     return function(city) {
         if (tab.includes(city)) {
-            return city + " is a part of " + country
+            return city + " is a city of " + country
         } else {
-            return city + " is not a part of " + country
+            return city + " is not a city of " + country
         }
     }
 }
