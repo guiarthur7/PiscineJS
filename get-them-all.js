288eda224ddad1cfb0f28ae76b4fa6bb59fee5fd
guiarthur <arthur.guillaume@ynov.com>
2026-03-20 11:12:03 +0100

    msg

--- get-them-all.js
+++ get-them-all.js
@@ -1,6 +1,6 @@
-export function getArchitects() {
+export const getArchitects = () => {
     const allElements = Array.from(document.body.children);
-    const architects = allElements.filter(el => el.tagName === "A")
-    const nonArchitects = allElements.filter(el => el.tagName !== 'A');
+    const architects = allElements.filter(el => el.tagName === "A");
+    const nonArchitects = allElements.filter(el => el.tagName !== "A");
     return [architects, nonArchitects];
-}
+};
