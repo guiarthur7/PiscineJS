3e165cf71ccc37041ba9392a62680465fa407fef
guiarthur <arthur.guillaume@ynov.com>
2026-03-20 11:09:59 +0100

    msg

--- get-them-all.js
+++ get-them-all.js
@@ -1,6 +1,6 @@
 function getArchitects() {
     const allElements = Array.from(document.body.children);
-    const architects = allElements.filter(el => el.tagName == "a")
+    const architects = allElements.filter(el => el.tagName === "A")
     const nonArchitects = allElements.filter(el => el.tagName !== 'A');
     return [architects, nonArchitects];
 }
