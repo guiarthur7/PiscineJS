a11791da8a357291a55cc7b2507cc2861829ab6d
guiarthur <arthur.guillaume@ynov.com>
2026-03-20 11:24:27 +0100

    msg

--- get-them-all.js
+++ get-them-all.js
@@ -1,8 +1,27 @@
-function getArchitects() {
+export function getArchitects() {
     const allElements = Array.from(document.body.children);
     const architects = allElements.filter(el => el.tagName === "A")
     const nonArchitects = allElements.filter(el => el.tagName !== 'A');
     return [architects, nonArchitects];
 }
 
-export {getArchitects}
+export function getClassical() {
+    const architects = Array.from(document.getElementsByTagName('a'));
+    const classical = architects.filter(el => el.classList.contains("classical"));
+    const notClassical = architects.filter(el => !el.classList.contains("classical"));
+    return [classical, notClassical];
+}
+
+export function getActive() {
+    const classicalA = getClassical()[0]
+    const isActive = classicalA.filter(el => el.classList.contains("active"))
+    const isNotActive = classicalA.filter(el => !el.classList.contains("active"))
+    return [isActive, isNotActive];
+}
+
+export function getBonannoPisano() {
+    const classicalAactive = getActive()[0]
+    const isBonanno = classicalAactive.filter(el => el.id === "BonannoPisano")
+    const isNotBonanno = classicalAactive.filter(el => !el.id !== "BonannoPisano")
+    return [isBonanno, isNotBonanno];
+}
