a4c59bcbb4f423155ac29d65900b47b2e8d6b5e0
guiarthur <arthur.guillaume@ynov.com>
2026-03-20 11:08:00 +0100

    msg

--- get-them-all.js
+++ get-them-all.js
@@ -1,11 +1,6 @@
-// Retourne un tableau contenant deux tableaux :
-// [ [architects], [non-architects (un sur deux)] ]
-export const getArchitects = () => {
-    // Tous les <a>
-    const architects = Array.from(document.getElementsByTagName('a'));
-    // Tous les enfants du body
+function getArchitects() {
     const allElements = Array.from(document.body.children);
-    // Non-architectes : un sur deux (index impair)
-    const nonArchitects = allElements.filter((el, idx) => idx % 2 === 1);
+    const architects = allElements.filter(el => el.tagName == "a")
+    const nonArchitects = allElements.filter(el => el.tagName !== 'A');
     return [architects, nonArchitects];
-};
+}
