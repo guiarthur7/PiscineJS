0f2dab785746b865edb07871f6fa6463a3fe5ead
guiarthur <arthur.guillaume@ynov.com>
2026-03-22 19:33:11 +0100

    msg

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,5 +1,7 @@
+let cpt = 1;
+
 export function build(nbBrique) {
-    let cpt = 1
+    let cpt1 = 1
     const interval = setInterval(() => {
         if (cpt > nbBrique) {
             clearInterval(interval)
@@ -7,10 +9,11 @@ export function build(nbBrique) {
         }
         const div = document.createElement("div");
         div.id = `brick-${cpt}`
-        if (cpt%2===0) {
+        if (cpt%3===1) {
             div.setAttribute('foundation', 'true')
         }
         document.body.appendChild(div);
+        cpt1 += 1;
         cpt += 1;
     }
     , 100);
@@ -25,4 +28,10 @@ export function repair(ids) {
             brick.setAttribute('repaired', 'true')
         }
     });
+}
+
+export function destroy() {
+    const div = document.getElementById(`brick-${cpt}`)
+    div.remove()
+    cpt -= 1
 }
