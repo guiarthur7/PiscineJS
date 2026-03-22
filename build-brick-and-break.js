c8866d726c74a57ae58cdfd8bd92f96a4a8d72cc
guiarthur <arthur.guillaume@ynov.com>
2026-03-22 19:55:46 +0100

    msg

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -21,10 +21,12 @@ export function build(nbBrique) {
 
 export function repair(...ids) {
   ids.flat().forEach(id => {
-    const brick = document.getElementById(id);
+    const brick = document.getElementById(`brick-${id}`);
     if (!brick) return;
     if (brick.dataset.foundation === "true") {
       brick.dataset.repaired = 'in progress';
+    } else {
+        brick.dataset.repaired = 'repaired';
     }
   });
 }
