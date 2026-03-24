058542db8e161998246c843fd413b29c2f462e20
guiarthur <arthur.guillaume@ynov.com>
2026-03-24 09:36:58 +0100

    msg

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -2,8 +2,19 @@ export function generateLetters() {
     let size = 11;
     let result = [];
     for (let i = 1; i <= 120; i++) {
-        const letter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-        result.push({ letter, size });
+        const lettre = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+        let div = document.createElement("div");
+        div.textContent = lettre;
+        div.style.fontSize = `${size}px`;
+        if (i >= 1 && i <= 40) {
+            div.style.fontWeight = "300";
+        } else if (i > 40 && i <= 80) {
+            div.style.fontWeight = "400";
+        } else {
+            div.style.fontWeight = "600";
+        }
+        document.body.appendChild(div);
+        result.push(div);
         size += 1;
     }
     return result;
