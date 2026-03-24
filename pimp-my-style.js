033709ab2b1027c5033d1c1ab93e4dfe2172e4c3
guiarthur <arthur.guillaume@ynov.com>
2026-03-24 11:18:05 +0100

    msg

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -2,15 +2,14 @@ import { stylestableau } from './pimp-my-style.data.js';
 
 let cpt = 0;
 let cpt2 = 0;
-
 export function pimp() {
-    const btn = document.querySelector('button');
+    const btn = document.querySelector('.button');
     if (cpt < stylestableau.length) {
         btn.classList.add(stylestableau[cpt]);
         cpt += 1;
         cpt2 = cpt;
         if (cpt === stylestableau.length) {
-            btn.classList.add("unpimp");
+            btn.classList.add('unpimp');
         }
     } else {
         if (cpt2 > 0) {
@@ -18,7 +17,7 @@ export function pimp() {
             cpt2 -= 1;
         }
         if (cpt2 === 0) {
-            btn.classList.remove("unpimp");
+            btn.classList.remove('unpimp');
             cpt = 0;
         }
     }
