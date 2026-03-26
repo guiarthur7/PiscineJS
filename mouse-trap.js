f50ae678d33b91aac7285b26d532e4fa833b1dba
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 11:51:37 +0100

    msg

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,23 +1,29 @@
-document.addEventListener("click", (e) => {
-    let x = e.clientX;
-    let y = e.clientY;
-    creatCircle(x, y)
-})
+document.addEventListener('click', (e) => {
+    createCircle(e.clientX, e.clientY);
+});
 
-export function creatCircle(x, y) {
-    const div = document.createElement('div')
-    div.classList.add('circle')
-    div.style.background = 'white'
+document.addEventListener('mousemove', (e) => {
+    moveCircle(e.clientX, e.clientY);
+});
+
+export function createCircle(x, y) {
+    const div = document.createElement('div');
+    div.classList.add('circle');
+    div.style.background = 'white';
     div.style.position = 'absolute';
     div.style.left = x + 'px';
     div.style.top = y + 'px';
-    document.body.appendChild(div)
+    document.body.appendChild(div);
 }
 
-export function moveCircle() {
-
+export function moveCircle(x, y) {
+    const cir = document.getElementsByTagName("div")[0];
+    if (cir) {
+        cir.style.left = x + 'px';
+        cir.style.top = y + 'px';
+    }
 }
 
 export function setBox() {
-
+    
 }
