7a2b072d9fd92929b7afcb9918c2a5e3013286f3
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 09:42:49 +0100

    msg

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,20 +1,20 @@
-import { stylestableau } from './pimp-my-style.data.js';
+import { styles } from './pimp-my-style.data.js';
 
 let cpt = 0;
 let cpt2 = 0;
 export function pimp() {
     const btn = document.querySelector('.button');
 
-    if (cpt < stylestableau.length) {
-        btn.classList.add(stylestableau[cpt]);
+    if (cpt < styles.length) {
+        btn.classList.add(styles[cpt]);
         cpt++;
         cpt2 = cpt;
-        if (cpt === stylestableau.length) {
+        if (cpt === styles.length) {
             btn.classList.add('unpimp');
         }
     } else {
         if (cpt2 > 0) {
-            btn.classList.remove(stylestableau[cpt2 - 1]);
+            btn.classList.remove(styles[cpt2 - 1]);
             cpt2--;
             if (cpt2 === 0) {
                 btn.classList.remove('unpimp');
