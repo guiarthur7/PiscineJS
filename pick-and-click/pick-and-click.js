96588a869fcdc07ab627ae2f97b685372e379f9a
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 21:49:07 +0100

    msg

--- pick-and-click/pick-and-click.js
+++ pick-and-click/pick-and-click.js
@@ -1,5 +1,5 @@
 document.addEventListener("mousemove", (e) => {
-    pick(e.screenX, e.screenY, window.innerWidth, window.innerWidth)
+    pick(e.clientX, e.clientY, window.innerWidth, window.innerHeight)
 })
 
 export function pick(x, y, w, h) {
@@ -13,17 +13,15 @@ export function pick(x, y, w, h) {
     let str = `hsl(${hue}, 50%, ${luminosity}%)`
     document.body.style.backgroundColor = str;
 
-    const div1 = document.createElement('div')
-    div1.classList.add('hsl')
-    div1.textContent(str)
-
-    const div2 = document.createElement('div')
-    div2.classList.add('hue text')
-    div2.textContent(`${hue}`)
-
-    const div3 = document.createElement('div')
-    div3.classList.add("luminosity text")
-    div2.textContent(`${luminosity}`)
+    if (document.querySelector(".hsl")) {
+        document.querySelector(".hsl").textContent = str;
+    }
+    if (document.querySelector(".hue.text")) {
+        document.querySelector(".hue.text").textContent = `${hue}`;
+    }
+    if (document.querySelector(".luminosity.text")) {
+        document.querySelector(".luminosity.text").textContent = `${luminosity}`;
+    }
 
     navigator.clipboard.writeText(str)
     .then(() => {
