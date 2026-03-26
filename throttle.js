1f83e667af86cf7674a00bf3a071f542b7147e8c
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 23:22:16 +0100

    msg

--- throttle.js
+++ throttle.js
@@ -9,3 +9,31 @@ function throttle(func, time) {
         }, time)
     }
 }
+
+function opThrottle(func, time, options = {leading: false, trailing: false,}) {
+    const { leading, trailing } = options
+    let waiting = false
+    let firstCalled = false
+
+    return function(...arg) {
+        if (leading && !firstCalled) {
+            func(...arg)
+            firstCalled = true
+            waiting = true
+            setTimeout(() => {
+                waiting = false
+            }, time)
+            return
+        }
+        if (waiting) return
+
+        func(...arg)
+        waiting = true
+        setTimeout(() => {
+            waiting = false
+            if (trailing) {
+                func()
+            }
+        }, time)
+    }
+}
