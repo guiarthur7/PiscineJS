a39931b0cbb3b30382ffbed4ad6903a1be6b3618
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 09:24:45 +0100

    msg

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -4,12 +4,11 @@ let cpt = 0;
 let cpt2 = 0;
 export function pimp() {
     const btn = document.querySelector('button');
-    if (!btn) {
-        return
-    }
+    if (!btn) return;
+
     if (cpt < stylestableau.length) {
         btn.classList.add(stylestableau[cpt]);
-        cpt += 1;
+        cpt++;
         cpt2 = cpt;
         if (cpt === stylestableau.length) {
             btn.classList.add('unpimp');
@@ -17,11 +16,11 @@ export function pimp() {
     } else {
         if (cpt2 > 0) {
             btn.classList.remove(stylestableau[cpt2 - 1]);
-            cpt2 -= 1;
-        }
-        if (cpt2 === 0) {
-            btn.classList.remove('unpimp');
-            cpt = 0;
+            cpt2--;
+            if (cpt2 === 0) {
+                btn.classList.remove('unpimp');
+                cpt = 0;
+            }
         }
     }
 }
