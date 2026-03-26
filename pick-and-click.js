2eb2536fd8b9ec3da9ab28e719807bf684d178f4
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 22:09:22 +0100

    msg

--- pick-and-click.js
+++ pick-and-click.js
@@ -1,6 +1,17 @@
-document.addEventListener("mousemove", (e) => {
-    pick(e.clientX, e.clientY, window.innerWidth, window.innerHeight)
-})
+function hslToRgb(h, s, l) {
+    s /= 100; l /= 100;
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = n => l - a * Math.max(-1, Math.min(Math.min(k(n) - 3, 9 - k(n)), 1));
+    return [Math.round(255 * f(0)), Math.round(255 * f(8)), Math.round(255 * f(4))];
+}
+
+function handleMove(e) {
+    pick(e.clientX, e.clientY, window.innerWidth, window.innerHeight);
+}
+
+document.addEventListener("mousemove", handleMove);
+window.addEventListener("pointermove", handleMove);
 
 function pick(x, y, w, h) {
     const svg = document.getElementById('crosshairs');
@@ -11,14 +22,6 @@ function pick(x, y, w, h) {
     const luminosity = Math.round((y / h) * 100);
 
     let str = `hsl(${hue}, 50%, ${luminosity}%)`
-
-    function hslToRgb(h, s, l) {
-        s /= 100; l /= 100;
-        const k = n => (n + h / 30) % 12;
-        const a = s * Math.min(l, 1 - l);
-        const f = n => l - a * Math.max(-1, Math.min(Math.min(k(n) - 3, 9 - k(n)), 1));
-        return [Math.round(255 * f(0)), Math.round(255 * f(8)), Math.round(255 * f(4))];
-    }
 
     const [r, g, b] = hslToRgb(hue, 50, luminosity);
     document.body.style.background = `rgb(${r},${g},${b})`;
