f9374af1c596ffe85b0e5417aa8765d18fad635a
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 11:00:42 +0100

    msg

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -4,15 +4,13 @@ document.addEventListener("keydown", (event) => {
 
 function compose(event) {
     const key = event.key;
-
-    if (key.length === 1 && key >= 'a' && key <= 'z') {
+    if (key.length === 1 && ((key >= 'a' && key <= 'z') || (key >= 'A' && key <= 'Z'))) {
         const div = document.createElement('div');
         div.classList.add('note');
         div.textContent = key;
         const color = '#' + (key.charCodeAt(0) * 1234567 % 0xFFFFFF).toString(16).padStart(6, '0');
         div.style.background = color;
         document.body.appendChild(div);
-
     } else if (key === "Backspace") {
         const notes = document.getElementsByClassName('note');
         if (notes.length > 0) {
