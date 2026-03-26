0554e287d140a4cc352ffbb2bfff93e225029ee4
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 22:59:54 +0100

    msg

--- throttle.js
+++ throttle.js
@@ -1,9 +1,10 @@
 function throttle(func, time) {
     let wait = false
     return function(...arg) {
+        if (wait) return
         func(...arg)
         wait = true
-        setTimeout( () => {
+        setTimeout(() => {
             wait = false
         }, time)
     }
