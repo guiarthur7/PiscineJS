eb88de90ed22f371593b70d0914368ed6c6e86a7
guiarthur <arthur.guillaume@ynov.com>
2026-03-26 22:51:08 +0100

    msg

--- debounce.js
+++ debounce.js
@@ -1,11 +1,29 @@
-function debounce(func, time) {
-    setTimeout(func, time)
+function debounce(func, delay) {
+    let timeout
+    return function (...args) {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            func(...args)
+        }, delay)
+    }
 }
 
-function opDebounce(func, time, lead) {
-    if (lead === true) {
-        func()
-        lead = false
+function opDebounce(func, time, option = { leading : false}) {
+    let timer = null
+    let called = false
+    let { leading } = option
+    if (leading == true && called == false) {
+        return function(...args) {
+            func(...args)
+            called = true
+        }
+    } else {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            called = false
+            if (!leading) {
+                func(...args);
+            }
+        }, time);
     }
-    setTimeout(func, time)
 }
