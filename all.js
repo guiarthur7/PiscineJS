4e5f0f164fd8084ef64b0f83ebc800466b25ea38
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 19:40:44 +0100

    msg

--- all.js
+++ all.js
@@ -1,6 +1,16 @@
 export function all(obj) {
 	const entries = Object.entries(obj);
-	// Utilise toujours la Promise globale native
-	return globalThis.Promise.all(entries.map(async ([k, v]) => [k, await v]))
-		.then(Object.fromEntries);
+	return new Promise((resolve, reject) => {
+		let count = entries.length;
+		const results = [];
+		if (count === 0) return resolve({});
+		entries.forEach(([k, v], i) => {
+			Promise.resolve(v)
+				.then(val => {
+					results[i] = [k, val];
+					if (--count === 0) resolve(Object.fromEntries(results));
+				})
+				.catch(reject);
+		});
+	});
 }
