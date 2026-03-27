dd899d83d6b34daa5af2530b5455db1bec7fa7b4
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 15:41:37 +0100

    msg

--- tell-it-cypher.mjs
+++ tell-it-cypher.mjs
@@ -2,12 +2,19 @@ import { readFileSync, writeFileSync } from "fs"
 
 const first = process.argv[2] || "."
 const second = process.argv[3] || "."
+const output = process.argv[4] || "."
 const r = readFileSync(first, "utf8")
+
+if (outpout.length <= 2 && String(second).toLowerCase() === "encode") {
+    output = "cypher.txt"
+} if (outpout.length <= 2 && String(second).toLowerCase() === "decode") {
+    output = "clear.txt"
+}
 
 if (String(second).toLowerCase() === "encode") {
     let encode = Buffer.from(r).toString('base64')
-    writeFileSync("cypher.txt", encode)
+    writeFileSync(output, encode)
 } else if (String(second).toLowerCase() === "decode") {
     let decode = Buffer.from(r, 'base64').toString('utf8')
-    writeFileSync("clear.txt", decode)
+    writeFileSync(output, decode)
 }
