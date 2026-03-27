acbf87160a04bc8b485a4f19e594fd5a6bd2e97f
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 19:34:42 +0100

    msg

--- all.js
+++ all.js
@@ -1,5 +1,6 @@
 export function all(obj) {
 	const entries = Object.entries(obj);
-	return Promise.all(entries.map(async ([k, v]) => [k, await v]))
+	const P = (typeof Promise === 'function' ? Promise : globalThis.Promise);
+	return P.all(entries.map(async ([k, v]) => [k, await v]))
 		.then(Object.fromEntries);
 }
