441a8d0bbc815e896cfbab827e7f6d2974facb4d
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 08:49:16 +0100

    msg

--- pick-and-click.js
+++ pick-and-click.js
@@ -17,10 +17,33 @@ function pick(x, y, w, h) {
     const hue = Math.round((x / w) * 360);
     const luminosity = Math.round((y / h) * 100);
 
-    const str = `hsl(${hue}, 50%, ${luminosity}%)`;
+    const hslStr = `hsl(${hue}, 50%, ${luminosity}%)`;
+    const s = 50 / 100;
+    const l = luminosity / 100;
 
-    document.body.style.background = str;
-    currentHsl = str;
+    function hslToRgb(h, s, l) {
+        const c = (1 - Math.abs(2 * l - 1)) * s;
+        const hh = h / 60;
+        const x = c * (1 - Math.abs((hh % 2) - 1));
+        let r1 = 0, g1 = 0, b1 = 0;
+        if (hh >= 0 && hh < 1) { r1 = c; g1 = x; b1 = 0; }
+        else if (hh >= 1 && hh < 2) { r1 = x; g1 = c; b1 = 0; }
+        else if (hh >= 2 && hh < 3) { r1 = 0; g1 = c; b1 = x; }
+        else if (hh >= 3 && hh < 4) { r1 = 0; g1 = x; b1 = c; }
+        else if (hh >= 4 && hh < 5) { r1 = x; g1 = 0; b1 = c; }
+        else { r1 = c; g1 = 0; b1 = x; }
+        const m = l - c / 2;
+        const r = Math.round((r1 + m) * 255);
+        const g = Math.round((g1 + m) * 255);
+        const b = Math.round((b1 + m) * 255);
+        return { r, g, b };
+    }
+
+    const { r, g, b } = hslToRgb(hue, s, l);
+    const rgbStr = `rgb(${r}, ${g}, ${b})`;
+
+    document.body.style.background = rgbStr;
+    currentHsl = rgbStr;
 
     const hslDiv = document.querySelector('.hsl');
     if (hslDiv) hslDiv.textContent = str;
