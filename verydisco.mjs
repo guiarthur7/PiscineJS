703b7fbfbb7eefa7208d4c2c2b01dbc7e1fefd7e
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 11:36:18 +0100

    msg

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,6 +1,11 @@
 const first = process.argv[2];
-if (first.length%2 !== 0) {
-    console.log(first.slice((first.length/2) + 1) + first.slice(0, first.length/2 + 1))
-} else {
-    console.log(first.slice((first.length/2)) + first.slice(0, first.length/2))
+let words = first.split(" ")
+let res = ""
+for (let i = 0; i < words.length; i++) {
+    if (words[i].length%2 !== 0) {
+        res += words[i].slice((first.length/2) + 1) + first.slice(0, first.length/2 + 1)
+    } else {
+        res += words[i].slice((first.length/2)) + first.slice(0, first.length/2)
+    }
+    console.log(res)
 }
