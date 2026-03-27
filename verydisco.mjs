25225caea908a62393a403615a37c50dc7f1e687
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 11:46:12 +0100

    msg

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,13 +1,11 @@
 const first = process.argv.slice(2).join(' ');
-console.log(first)
 let words = first.split(" ")
-console.log(words)
 let res = ""
 for (let i = 0; i < words.length; i++) {
     if (words[i].length%2 !== 0) {
-        res += words[i].slice((words[i].length/2) + 1) + words[i].slice(0, words[i].length/2 + 1) + " "
-    } else {
         res += words[i].slice((words[i].length/2)) + words[i].slice(0, words[i].length/2) + " "
+    } else {
+        res += words[i].slice((words[i].length/2) + 1) + words[i].slice(0, (words[i].length/2) + 1) + " "
     }
 }
 console.log(res)
