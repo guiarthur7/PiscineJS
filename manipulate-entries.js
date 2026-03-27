5bd4e467b9545be6b5d8755a9182f108bb3afa5e
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 10:07:50 +0100

    msg

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -7,3 +7,11 @@ function filterEntries(obj, fn) {
     }
     return res
 }
+
+function mapEntries(obj, fn) {
+    let res = {}
+    for (const [key, value] of Object.entries(obj)) {
+        res[fn([key, value])[0]] = fn([key, value])[1]
+    }
+    return res
+}
