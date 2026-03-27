c5037cdc0eb4c3c0b0897fe2120a2e956b4562fd
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 10:35:35 +0100

    msg

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -32,3 +32,43 @@ function reduceEntries(obj, fn) {
     }
     return acc;
 }
+
+const nutritionDB = {
+  tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+  vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+  oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+  onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+  garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+  paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+  sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+  orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+}
+
+function totalCalories(obj) {
+    return reduceEntries(obj, (acc, [name, grams]) => {
+        const per100 = nutritionDB[name].calories
+        return acc + per100 * (grams / 100);
+    }, 0);
+}
+
+function lowCarbs(obj) {
+    return filterEntries(obj, ([name, grams]) => {
+        const per100 = nutritionDB[name]?.carbs ?? 0;
+        const carbs = per100 * (grams / 100);
+        return carbs < 50;
+    });
+}
+
+function cartTotal(obj) {
+    return mapEntries(obj, ([name, grams]) => {
+        let res = {}
+        res[calories] = nutritionDB[name].calories *(grams/100)
+        res[protein] = nutritionDB[name].protein *(grams/100)
+        res[carbs] = nutritionDB[name].carbs *(grams/100)
+        res[sugar] = nutritionDB[name].sugar *(grams/100)
+        res[fiber] = nutritionDB[name].fiber *(grams/100)
+        res[fat] = nutritionDB[name].fat *(grams/100)
+
+        return [name, res]
+    })
+}
