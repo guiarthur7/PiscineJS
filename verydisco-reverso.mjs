5932881e33c83fd1d96efbeba00de13bcce87305
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 12:05:13 +0100

    msg

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -5,7 +5,7 @@ let words = text.split(" ")
 let res = ""
 for (let i = 0; i < words.length; i++) {
     if (words[i].length % 2 !== 0) {
-        res += words[i].slice((words[i].length / 2) + 1) + words[i].slice(0, (words[i].length / 2) + 1) + ' '
+        res += words[i].slice((words[i].length / 2)) + words[i].slice(0, (words[i].length / 2)) + ' '
     } else {
         res += words[i].slice((words[i].length / 2)) + words[i].slice(0, words[i].length / 2) + ' '
     }
