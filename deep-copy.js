61a256a8985b123e9a67727608967e1241442b03
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 11:05:30 +0100

    msg

--- deep-copy.js
+++ deep-copy.js
@@ -1,5 +1,6 @@
 function deepCopy(obj) {
     if (obj === null || typeof obj !== 'object') return obj;
+    if (obj instanceof RegExp) return new RegExp(obj.source, obj.flags);
     if (Array.isArray(obj)) {
         const out = [];
         for (let i = 0; i < obj.length; i++) out[i] = deepCopy(obj[i]);
