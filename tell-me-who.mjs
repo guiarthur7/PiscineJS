7f1a4cfd26fa1e624f68208fb45b2db2c61b4dba
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 12:51:57 +0100

    msg

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -7,9 +7,11 @@ const names = [];
 for (const f of files) {
   const base = f.split('.')[0];
   const parts = base.split('_');
-  if (parts.length === 2) names.push(parts[1] + ' ' + parts[0]);
-  else names.push(base);
+  if (parts.length === 2) {
+    names.push(parts[1] + ' ' + parts[0]);
+  } else {
+    names.push(base);
+  }
 }
-names.sort((a, b) => a.localeCompare(b, 'en'));
 
 names.forEach((n, i) => console.log(`${i + 1}. ${n}`));
