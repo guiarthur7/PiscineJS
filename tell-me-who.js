7f763adfb208719eb26849f8a2d2587fe9bd2490
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 12:12:00 +0100

    msg

---
+++ tell-me-who.js
@@ -0,0 +1,16 @@
+import { readdir, readFile } from "node:fs/promises";
+
+try {
+  const dirPath = process.argv[2];
+  if (!dirPath) throw new Error("Please provide a directory path.");
+
+  const files = await readdir(dirPath);
+  const guests = await Promise.all(
+    files.map((file) => readFile(`${dirPath}/${file}`, "utf8")),
+  );
+
+  const sortedGuests = guests.sort();
+  sortedGuests.forEach((guest, i) => console.log(`${i + 1}. ${guest}`));
+} catch (err) {
+  console.error("Error:", err.message);
+}
