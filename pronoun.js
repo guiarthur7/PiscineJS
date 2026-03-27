c53db832266548196345a861e1ba4b8315d62d0e
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 11:25:55 +0100

    msg

--- pronoun.js
+++ pronoun.js
@@ -1,24 +1,29 @@
-function pronoun(s) {
-    const pronouns = new Set([
-        'je','toi','il','elle','ils','nous',
-        'i','you','he','she','they','we'
-    ]);
+let pronouns = ['i','you','he','she','it','they','we']
 
-    const words = (s.match(/[A-Za-z]+/g) || []).map(w => w.toLowerCase());
-    const out = {};
+const words = (arr,ind) => {
+        if (!pronouns.includes(arr[ind]))
+            return arr[ind]
+        return ''
+}
+function splitString(input) {
+    return input.split(/[\s,]+/).map(word => word.trim()).filter(word => word !== '');
+}
 
-    for (let i = 0; i < words.length; i++) {
-        const w = words[i];
-        if (!pronouns.has(w)) continue;
-        if (!out[w]) out[w] = { wordSet: new Set(), count: 0 };
-        out[w].count += 1;
-        const next = words[i+1];
-        if (next && !pronouns.has(next)) out[w].wordSet.add(next);
+function pronoun(string){
+    let arr = splitString(string)
+    let rtn = {}
+    for (let i = 0;i< arr.length;i++){
+        if (pronouns.includes(arr[i].toLowerCase())){
+            let wwor = words(arr,i+1)
+            if (!rtn[arr[i].toLowerCase()]) {
+                rtn[arr[i].toLowerCase()] = {word:[],count:1}
+                if (wwor != '' && wwor != undefined) rtn[arr[i].toLowerCase()].word.push(wwor)
+            }
+            else {
+                if (wwor != '') rtn[arr[i].toLowerCase()].word.push(wwor)
+                rtn[arr[i].toLowerCase()].count++
+            }
+        }
     }
-
-    const res = {};
-    for (const [k, v] of Object.entries(out)) {
-        res[k] = { word: Array.from(v.wordSet), count: v.count };
-    }
-    return res;
+    return rtn
 }
