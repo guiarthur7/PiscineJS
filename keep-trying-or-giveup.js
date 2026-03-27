d839a603d3f7a298ee2a85032d66c5edfa617643
guiarthur <arthur.guillaume@ynov.com>
2026-03-27 19:21:01 +0100

    msg

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,7 +1,23 @@
-function retry(count, callback) {
-
+export function retry(count, callback) {
+	return async function (...args) {
+		let lastError;
+		for (let attempt = 0; attempt <= count; attempt++) {
+			try {
+				return await callback(...args);
+			} catch (err) {
+				lastError = err;
+			}
+		}
+		throw new Error('All retries failed');
+	};
 }
 
-function timeout(delay, callback) {
-    
+// timeout: exécute callback, rejette si délai dépassé
+export function timeout(delay, callback) {
+	return async function (...args) {
+		return Promise.race([
+			callback(...args),
+			new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), delay))
+		]);
+	};
 }
